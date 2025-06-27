ad303cb567c59cd03f92da7bda642857c87504ef
Gabriel Spioni <gabrielprivate123@gmail.com>
2025-06-27 19:44:39 -0300

    Revert "fix: Tornando método de cálculo de tempo genérico."
    This reverts commit d713fae198fadf683b956fb67a7f0f2fb9176b4b.

--- processing/update_time_processor.py
+++ processing/update_time_processor.py
@@ -1,17 +1,19 @@
 import time
 from utils.constants import YELLOW, GREEN, RESET
 
-def update_processing_time(process_name, shared_controls, start_time, total_time, frame_count, log_interval=100):
+def update_processing_time(shared_controls, start_time, total_time, frame_count, log_interval=100):
     end_time = time.time()
-    frame_time = (end_time - start_time) * 1000  # ms
+    frame_time = (end_time - start_time) * 1000  # em milissegundos
     total_time += frame_time
     frame_count += 1
 
     fps = 1.0 / (end_time - start_time) if (end_time - start_time) > 0 else 0.0
     avg_time = total_time / frame_count
-
     if frame_count % log_interval == 0:
-        print(f"{YELLOW}[{process_name}]{GREEN}[INFO] Tempo médio por frame: {avg_time:.2f} ms (baseado em {frame_count} frames){RESET}")
-        print(f"{YELLOW}[{process_name}]{GREEN}[INFO] FPS: {fps:.2f}{RESET}")
-
+        print(
+            f"{YELLOW}[LaneDetection]{GREEN}[INFO] Tempo médio por frame: {avg_time:.2f} ms (baseado em {frame_count} frames){RESET}")
+        print(
+            f"{YELLOW}[LaneDetection]{GREEN}[INFO] FPS: {fps:.2f}.{RESET}"
+        )
     return frame_count, fps, avg_time, total_time
+
