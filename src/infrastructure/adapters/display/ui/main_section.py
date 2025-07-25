5e28581ed62b9eac9af7cb6096ab989f6e388f66
Gabriel Spioni <gabrielprivate123@gmail.com>
2025-07-25 17:44:52 -0300

    fix: Melhorando placeholder de vídeos.

--- src/infrastructure/adapters/display/ui/main_section.py
+++ src/infrastructure/adapters/display/ui/main_section.py
@@ -71,13 +71,17 @@ class VideoFrame(ctk.CTkFrame):
         self.shared_controls = shared_controls
         self.label = ctk.CTkLabel(self, text=title)
         self.label.pack()
-        self.image_label = ctk.CTkLabel(self, text="")
+
+        placeholder_img = Image.new("RGB", (FRAME_WIDTH_T, FRAME_HEIGHT_T), color=(50, 50, 50))
+        self.placeholder_ctk_image = CTkImage(light_image=placeholder_img, size=(FRAME_WIDTH_T, FRAME_HEIGHT_T))
+
+        self.image_label = ctk.CTkLabel(self, text="", image=self.placeholder_ctk_image)
         self.image_label.pack()
 
     def update_image(self, image_bytes):
         if self.shared_controls.get("WEBVIEW"):
-            self.image_label.configure(image="", text="Webview ATIVO.")
-            self.image_label.image = None
+            self.image_label.configure(image=self.placeholder_ctk_image, text="Webview ATIVO.")
+            self.image_label.image = self.placeholder_ctk_image
             return
         if image_bytes:
             image = Image.open(io.BytesIO(image_bytes)).resize((FRAME_WIDTH_T, FRAME_HEIGHT_T))
