19b813ca15131076dab0567c1bf4a365361af44e
Gabriel Spioni <gabrielprivate123@gmail.com>
2025-08-13 00:19:53 -0300

    fix: Correção de instância nativa para câmera.

--- src/infrastructure/adapters/video/video_process.py
+++ src/infrastructure/adapters/video/video_process.py
@@ -1,12 +1,14 @@
 import os
 import cv2
-from src.infrastructure.constants.video_constants import FRAME_WIDTH, FRAME_HEIGHT
+from src.infrastructure.constants.video_constants import FRAME_WIDTH, FRAME_HEIGHT, WIDE_CAPTURE_WIDTH, WIDE_CAPTURE_HEIGHT
 
 class VideoProcessor:
     def __init__(self, video_source, frame_width=FRAME_WIDTH, frame_height=FRAME_HEIGHT):
         self.video_source = video_source
-        self.frame_width = frame_width
-        self.frame_height = frame_height
+        self.output_width = frame_width
+        self.output_height = frame_height
+        self.internal_width = WIDE_CAPTURE_WIDTH
+        self.internal_height = WIDE_CAPTURE_HEIGHT
 
         self.is_cam = False
         self.cam_index = None
@@ -29,13 +31,14 @@ class VideoProcessor:
             raise RuntimeError(f"Não foi possível abrir a fonte de vídeo: {video_source}")
 
         if self.is_cam:
-            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
-            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.internal_width)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.internal_height)
 
         actual_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         actual_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
         self.resize_needed = (
-                actual_width != self.frame_width or actual_height != self.frame_height
+            actual_width != self.output_width or actual_height != self.output_height
         )
 
     def get_frame(self):
@@ -51,7 +54,8 @@ class VideoProcessor:
                 raise RuntimeError("Erro ao capturar frame da câmera")
 
         if self.resize_needed:
-            frame = cv2.resize(frame, (self.frame_width, self.frame_height))
+            frame = cv2.resize(frame, (self.output_width, self.output_height), interpolation=cv2.INTER_AREA)
+
         return frame
 
     def release(self):
