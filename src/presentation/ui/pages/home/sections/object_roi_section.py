eee3ccd40b0c89bbfc9ed3a4b2101b13580dff1a
Gabriel Estevão <gabrielprivate123@gmail.com>
2025-10-22 23:40:22 -0300

    fix: Correção de altura de seção e ordenação dos sliders.

--- src/presentation/ui/pages/home/sections/object_roi_section.py
+++ src/presentation/ui/pages/home/sections/object_roi_section.py
@@ -6,9 +6,9 @@ class ObjectRoiSection(SliderSection):
         sliders = [
             SliderConfig("Person", "Person", 0, 240),
             SliderConfig("Traffic", "Traffic Sign", 0, 240),
+            SliderConfig("PeopleRegion", "People Region", 10, 100),
             SliderConfig("PLACA_PARE", "Placa Pare", 0, 240),
             SliderConfig("PLACA_DESVIO", "Placa Desvio", 0, 240),
             SliderConfig("PLACA_LOMBADA", "Placa Lombada", 0, 240),
-            SliderConfig("PeopleRegion", "People Region", 10, 100),
         ]
         super().__init__(master, "ROI de Objetos", tk_controls, calibration_data, sliders, **kwargs)
